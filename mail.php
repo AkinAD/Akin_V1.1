f8c8c727837506a113617a837b5fbf293410f94c
Akin Ade <mrspence004@gmail.com>
2019-08-02 16:40:49 -0400

    Initial Commit
    Missing a few things here and there but initial commit for ade-ak.in

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $formcontent = "From: $name \n Message: $message";
+  $recipient = "akin.pjadewale@yahoo.com";
+  $subject = "Message From Akins Website";
+  $mailheader = "From: $email \r\n";
+  mail($recipient, $subject, $formcontent, $mailheader) or die("Error! You have failed!");
+  echo "Success! You'll hear from us soon!";
+  ?>
